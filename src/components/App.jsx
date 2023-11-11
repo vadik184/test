e6a715d9014268e9db9c11d514df302e51aa872b
vadik184 <133372327+vadik184@users.noreply.github.com>
2023-11-11 16:25:04 +0200

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,19 @@
-import { Counter } from '../components/Counter/Counter';
+import { SingUpForm } from '../components/Counter/Counter';
 
 export const App = () => {
   return (
     <div
       style={{
         height: '100vh',
-        display: 'flex',
+        display: 'block',
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
         color: '#010101',
+        padding: 20,
       }}
     >
-      <Counter />
+      <SingUpForm />
     </div>
   );
 };
--- src/components/Counter/Counter.js
+++ src/components/Counter/Counter.js
@@ -1,11 +1,66 @@
-import React from "react";
+import React, { Component } from 'react';
 
-export const Counter = () => (
-  <div className="Counter">
-    <span className="Counter__value">0</span>
-    <div className="Counter__controls">
-      <button type="button">Збільшити на 1</button>
-      <button type="button">Зменшити на 1</button>
-    </div>
-  </div>
-);
+const INITIAL_STATE = {
+  login: '',
+  email: '',
+  password: '',
+};
+export class SingUpForm extends Component {
+  state = { ...INITIAL_STATE };
+
+  handelChange = e => {
+    const { name, value } = e.target;
+    this.setState({ [name]: value });
+  };
+
+  handelSubmit = evt => {
+    evt.preventDefault();
+    const { login, email, password } = this.state;
+    console.log(`Login: ${login}, Email: ${email}, Password: ${password}`);
+    console.log(`Signed up as: ${this.state.login}`);
+    this.props.onSubmit({ ...this.state });
+    this.reset();
+  };
+  reset = () => {
+    this.state({ ...INITIAL_STATE });
+  };
+
+  render() {
+    const { login, email, password } = this.state;
+    return (
+      <form onSubmit={this.handelSubmit}>
+        <label>
+          Name
+          <input
+            type="text"
+            placeholder="Enter login"
+            name="login"
+            value={login}
+            onChange={this.handelChange}
+          />
+        </label>
+        <label>
+          Email
+          <input
+            type="text"
+            placeholder="Enter login"
+            name="email"
+            value={email}
+            onChange={this.handelChange}
+          />
+        </label>
+        <label>
+          Password
+          <input
+            type="text"
+            placeholder="Enter login"
+            name="password"
+            value={password}
+            onChange={this.handelChange}
+          />
+        </label>
+        <button type="submit">Sing up as {login}</button>
+      </form>
+    );
+  }
+}
